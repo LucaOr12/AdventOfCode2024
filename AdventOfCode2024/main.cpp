1bf9f3d913ac058263a1d499df91ed9484195e08
Luca Orabona <luca.orabona1210@gmail.com>
2024-12-05 11:54:44 +0100

    Initial Commit

---
+++ AdventOfCode2024/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdventOfCode2024
+//
+//  Created by Luca Orabona on 05/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
